1a247350dbb57c3e05612029a7cc32391755fd40
Robbe Van Looy <robbe.vanlooy@student.hogent.be>
2021-01-25 13:09:57 +0100

    interaction cube and camera optimization
    Added new positioning  controls with synchronous camera movement.

--- interactiveSite/interactivePlaygroundLogic.js
+++ interactiveSite/interactivePlaygroundLogic.js
@@ -2,91 +2,101 @@ var scene, camera, renderer, cube;
 var meshFloor;
 
 var keyboard = {};
-var player = {
-  height: 1.8,
-  speed: 0.2,
-  turnSpeed: Math.PI * 0.02,
+
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 6;
+camera.position.y = 1;
+
+renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor("#e6fffd");
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshLambertMaterial({
+  color: "#a3d0ff",
+});
+var cube = new THREE.Mesh(geometry, material);
+
+var material2 = new THREE.MeshLambertMaterial({
+  color: "#ffa29c",
+});
+var cube2 = new THREE.Mesh(geometry, material2);
+scene.add(cube2);
+
+cube2.position.x = 5;
+
+scene.add(cube);
+
+var light = new THREE.PointLight(0xffffff, 1, 500);
+light.position.set(10, 0, 25);
+scene.add(light);
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+var render = function () {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+  animate();
 };
 
-function init() {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    10
-  );
+render();
 
-  var geometry = new THREE.BoxGeometry(1, 1, 1);
-  var material = new THREE.MeshBasicMaterial({
-    color: "white",
-    border: "black",
-  });
-  cube = new THREE.Mesh(geometry, material);
-  cube.position.y += 1; // Move the mesh up 1 meter
-  scene.add(cube);
-
-  meshFloor = new THREE.Mesh(
-    new THREE.PlaneGeometry(30, 30, 30, 30),
-    new THREE.MeshBasicMaterial({
-      color: "green",
-    })
-  );
-  meshFloor.height = 3;
-  meshFloor.rotation.x -= Math.PI / 2; // Rotate the floor 90 degrees
-  scene.add(meshFloor);
-
-  camera.position.set(0, player.height, -5);
-  camera.lookAt(new THREE.Vector3(0, player.height, 0));
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor("#e6fffd");
-  document.body.appendChild(renderer.domElement);
-
-  animate();
-}
+this.tl = new TimelineMax({ paused: true });
+this.tl.to(this.cube2.scale, 1, { x: 2, ease: Expo.easeOut });
+this.tl.to(this.cube2.scale, 0.5, { x: 0.5, ease: Expo.easeOut });
+this.tl.to(this.cube2.position, 0.5, { x: 2, ease: Expo.easeOut });
+this.tl.to(
+  this.cube2.rotation,
+  0.5,
+  { x: Math.PI * 0.5, ease: Expo.easeOut },
+  "=-1.5"
+);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  //cube.rotation.x += 0.01;
-  //cube.rotation.y += 0.02;
-
   // Keyboard movement inputs
   if (keyboard[87]) {
     // W key
-    cube.position.x -= Math.sin(cube.rotation.y) * player.speed;
-    cube.position.z -= -Math.cos(cube.rotation.y) * player.speed;
+    cube.position.z -= 0.0001;
+    camera.position.z -= 0.0001;
   }
   if (keyboard[83]) {
     // S keys
-    cube.position.x += Math.sin(cube.rotation.y) * player.speed;
-    cube.position.z += -Math.cos(cube.rotation.y) * player.speed;
+    cube.position.z += 0.0001;
+    camera.position.z += 0.0001;
   }
   if (keyboard[65]) {
     // A key
     // Redirect motion by 90 degrees
-    cube.position.x += Math.sin(cube.rotation.y + Math.PI / 2) * player.speed;
-    cube.position.z += -Math.cos(cube.rotation.y + Math.PI / 2) * player.speed;
+    //cube.rotation.y += 0.0003;
+    cube.position.x -= 0.0001;
+    camera.position.x -= 0.0001;
   }
   if (keyboard[68]) {
     // D key
-    cube.position.x += Math.sin(cube.rotation.y - Math.PI / 2) * player.speed;
-    cube.position.z += -Math.cos(cube.rotation.y - Math.PI / 2) * player.speed;
+    //cube.rotation.y -= 0.0003;
+    cube.position.x += 0.0001;
+    camera.position.x += 0.0001;
   }
 
   // Keyboard turn inputs
   if (keyboard[37]) {
     // left arrow key
-    camera.rotation.y -= player.turnSpeed;
+    camera.rotation.y -= 0.0001;
   }
   if (keyboard[39]) {
     // right arrow key
-    camera.rotation.y += player.turnSpeed;
+    camera.rotation.y += 0.0001;
   }
-
-  renderer.render(scene, camera);
 }
 
 function keyDown(event) {
@@ -99,5 +109,11 @@ function keyUp(event) {
 
 window.addEventListener("keydown", keyDown);
 window.addEventListener("keyup", keyUp);
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+
+  camera.updateProjectionMatrix();
+});
 
 window.onload = init;
